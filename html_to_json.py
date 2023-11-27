8f3028f6e8f0e12d802e2ac68e6d636dbaac0ce7
datledoan <dat.ld202595@sis.hust.edu.vn>
2023-11-27 14:46:03 +0700

    convert to world coordinate

--- html_to_json.py
+++ html_to_json.py
@@ -3,6 +3,7 @@ import json
 import re
 import os
 import argparse
+import yaml
 
 # Function to convert HTML table to JSON
 def html_table_to_json(html_filename, json_filename):
@@ -69,35 +70,68 @@ def add_start_end_ids(output_filtered_json):
         
         if item['Name'].startswith('Arc'):
             # search for start_id and end_id
-            center_name,start_name, end_name = item['Definition'].replace('Arc(', '').replace(')', '').split(', ')
+            origin_name,start_name, end_name = item['Definition'].replace('Arc(', '').replace(')', '').split(', ')
             start_id = name_to_id['Point ' + start_name]
             end_id = name_to_id['Point ' + end_name]
+            #origin_id = name_to_id['Point ' + origin_name]
 
             
             item['startid'] = start_id
             item['endid'] = end_id
+            #item['originid'] = origin_id
 
     
     with open(output_filtered_json, 'w') as json_file:
         json.dump(data, json_file, indent=2)
 
+#read size of image
+def read_pgm_dimensions(pgm_file_path):
+    with open(pgm_file_path, 'rb') as file:
+        # skip first line
+        file.readline()
+        
+        # read second line
+        size_line = file.readline().decode('utf-8').split()
+        width, height = map(int, size_line)
+
+    return width
+
+#read resolution and origin in map_info.yaml
+def read_yaml(yaml_file_path):
+    with open(yaml_file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        resolution = data.get('resolution')
+        origin = data.get('origin')
+    return resolution, origin
+
 
 # Format data to GeoJSON
-def convert_to_feature(point_data):
+def convert_to_feature(point_data, origin, ratio):
     name = point_data.get('Name', '')
     value = point_data.get('Value', '')
     id_counter = point_data.get('id', 0) + 1
+    
+
+
     #Point
     if name.startswith("Point"):
         
-        coordinates = [float(coord) for coord in re.findall(r'-?\d+\.\d+', value)]
+        coordinates = [float(coord) for coord in re.findall(r'-?\d+(?:\.\d+)?', value)]
 
+        if len(coordinates) == 2:
+            x_coordinate, y_coordinate = coordinates
+            x = float(x_coordinate) / ratio + origin[0]
+            y = float(y_coordinate) / ratio + origin[1]
+        else:
+            print("Error: Point coordinates must be 2D")
+        #print(x,y)
+        #print(f"{name}: ({x_coordinate}, {y_coordinate})")
         
         feature = {
             "type": "Feature",
             "geometry": {
                 "type": "Point",
-                "coordinates": coordinates
+                "coordinates": [x,y]
             },
             "properties": {
                 "id": id_counter,
@@ -108,10 +142,13 @@ def convert_to_feature(point_data):
     #Segment
     elif name.startswith("Segment"):
         
-        length = [float(coord) for coord in re.findall(r'-?\d+\.\d+', value)]
+        length = [float(coord) for coord in re.findall(r'-?\d+(?:\.\d+)?', value)]
+
+        length = length[0] / ratio
+
         start_id = point_data.get('startid', 0)
         end_id = point_data.get('endid', 0)
-        # Tạo cấu trúc mới
+
         feature = {
             "type": "Feature",
             "geometry": {
@@ -127,9 +164,10 @@ def convert_to_feature(point_data):
         }
     #Circlearc
     elif name.startswith("Arc"):
-        length = [float(coord) for coord in re.findall(r'-?\d+\.\d+', value)]
+        length = [float(coord) for coord in re.findall(r'-?\d+(?:\.\d+)?', value)]
         start_id = point_data.get('startid', 0)
         end_id = point_data.get('endid', 0)
+        #origin_id = point_data.get('originid',0)
         feature = {
             "type": "Feature",
             "geometry": {
@@ -140,21 +178,37 @@ def convert_to_feature(point_data):
                 "id": id_counter,
                 "frame": "map",
                 "startid": start_id,
-                "endid": end_id
+                "endid": end_id,
+                #"originid": origin_id
             }
         }
         return feature
     return None
 
-def graph_json(output_filtered_json,graph_file_path):
+def graph_json(output_filtered_json,graph_file_path, pgm_file_path, yaml_file_path):
+
+    # Read size of image
+    width = read_pgm_dimensions(pgm_file_path)
+    #read resolution and origin in map_info.yaml
+    resolution, origin = read_yaml(yaml_file_path)
+
+
     with open(output_filtered_json, 'r') as json_file:
         original_data = json.load(json_file)
 
-    # Chuyển đổi dữ liệu
+    for item in original_data:
+        if item['Name'] == 'Point B':
+            x = float(item['Value'].replace('B = (', '').replace(')', '').split(', ')[0])
+            break
+    
+    #ratio between geogebra and real world
+    ratio = x / (width*resolution)
+
+            
     features = []
 
     for data_point in original_data:
-        feature = convert_to_feature(data_point)
+        feature = convert_to_feature(data_point, origin, ratio)
         if feature:
             features.append(feature)
 
@@ -168,7 +222,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert HTML table to graph in JSON')
     parser.add_argument('--html_file', type=str, default='map1.html', help='Path to the HTML file')
     parser.add_argument('--graph_file_path', type=str, default='graph.json', help='Path to the graph JSON file')
-    parser.add_argument('--delete_unnecessary_files', type=bool, default=True, help='Delete unnecessary files')
+    parser.add_argument('--delete_unnecessary_files', type=bool, default=False, help='Delete unnecessary files')
+    parser.add_argument('--pgm_file_path', type=str, default='map.pgm', help='Path to the pgm file')
+    parser.add_argument('--yaml_file_path', type=str, default='map_info.yaml', help='Path to the yaml file')
     args = parser.parse_args()
 
 
@@ -176,11 +232,13 @@ if __name__ == '__main__':
     output_json_file = 'output.json'
     output_filtered_json = "filtered_feature.json"
     graph_file_path = args.graph_file_path
+    pgm_file_path = args.pgm_file_path
+    yaml_file_path = args.yaml_file_path
 
     html_table_to_json(html_file, output_json_file)
     filter(output_json_file,output_filtered_json)
     add_start_end_ids(output_filtered_json)
-    graph_json(output_filtered_json,graph_file_path)
+    graph_json(output_filtered_json,graph_file_path, pgm_file_path, yaml_file_path)
 
     if args.delete_unnecessary_files:
         os.remove(output_json_file)
