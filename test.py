b8cffb7f212d1a6de57c9744c8618ba2119764c5
datledoan <dat.ld202595@sis.hust.edu.vn>
2023-12-13 00:41:00 +0700

    test_convert_2_lap

--- test.py
+++ test.py
@@ -1,92 +1,79 @@
+import pandas as pd
 import json
+import re
+import os
+import argparse
+import yaml
 
-# Dữ liệu ban đầu
-data = [
-  {
-    "Name": "Point P",
-    "Definition": "fillet(D, F, E)",
-    "Value": "P = (13.3, 9.1)",
-    "id": 34
-  },
-  {
-    "Name": "Point Q",
-    "Definition": "fillet(D, F, E)",
-    "Value": "Q = (15.23, 9.11)",
-    "id": 35
-  },
-  {
-    "Name": "Point R",
-    "Definition": "fillet(D, F, E)",
-    "Value": "R = (13.29, 11.04)",
-    "id": 36
-  },
-  {
-    "Name": "Arc c1",
-    "Definition": "fillet(D, F, E)",
-    "Value": "c1 = 3.05",
-    "id": 37
-  },
-  {
-    "Name": "Segment b",
-    "Definition": "fillet(D, F, E)",
-    "Value": "b = 3.53",
-    "id": 38
-  },
-  {
-    "Name": "Segment f1",
-    "Definition": "fillet(D, F, E)",
-    "Value": "f1 = 1.94",
-    "id": 39
-  },
-  {
-    "Name": "Segment g1",
-    "Definition": "fillet(D, F, E)",
-    "Value": "g1 = 1.94",
-    "id": 40
-  },
-  {
-    "Name": "Segment h1",
-    "Definition": "fillet(D, F, E)",
-    "Value": "h1 = 5.31",
-    "id": 41
-  }
-]
 
-# Tìm các điểm có "Definition" là "fillet13(D, B, E)" hoặc "fillet13(V, W, Z)"
-arc_data = [item for item in data if "fillet" in item.get("Definition")]
-print(arc_data)
-# Tạo định dạng mới
-features = []
-for arc in arc_data:
-    if "Arc" in arc.get("Name"):
-      print(arc.get("id"))
-      start_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 2)
-      end_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 3)
-      origin_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 1)
+def dict_name_id(data,result_dict):
+    # Lấy giá trị từ trường "Name"
+    name = data.get('Name', '')
 
-      feature = {
-          "type": "Feature",
-          "geometry": {
-              "type": "Arc"
-          },
-          "properties": {
-              "id": arc["id"],
-              "startid": start_point["id"],
-              "endid": end_point["id"],
-              "originid": origin_point["id"],
-              "frame": "map"
-          }
-    }
+    # Tìm tất cả các ký tự và số trước dấu ngoặc
+    match = re.match(r'([^()]+)', name)
 
-    features.append(feature)
+    # Kiểm tra xem có kết quả từ việc tìm kiếm không
+    if match:
+        # Lấy phần match đầu tiên (đã loại bỏ dấu ngoặc)
+        key = match.group(1).strip()
+
+        # Lấy giá trị từ trường "id"
+        value = data.get('id')
+
+        # Thêm mục vào dictionary kết quả
+        result_dict[key] = value
+
+def process_data(data):
+    
+    features = []
+    
+    name_to_id = {}
+    for item in data:
+      dict_name_id(item,name_to_id)
+
+    print(name_to_id)
+
+    arc_data = [item for item in data if item.get("Definition") and "fillet" in item.get("Definition")]
+
+    for arc in arc_data:
+      if arc.get("Name").startswith("Arc"):
+        start_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 1)
+        end_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 2)
+        origin_point = next(item for item in arc_data if item.get("id") == arc.get("id") - 3)
+
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Arc"
+            },
+            "properties": {
+                "id": arc["id"],
+                "startid": start_point["id"],
+                "endid": end_point["id"],
+                "originid": origin_point["id"],
+                "frame": "map"
+            }
+        }
+
+        features.append(feature)
+
+    return features
+
+# Đọc dữ liệu từ tệp JSON
+with open("test.json", "r") as infile:
+    data = json.load(infile)
+
+# Xử lý dữ liệu và tạo định dạng mới
+new_features = process_data(data)
 
 # Xuất ra file JSON mới
 output_data = {
     "type": "FeatureCollection",
-    "features": features
+    "features": new_features
 }
 
-print(features)
+print(new_features)
 
 # with open("output.json", "w") as outfile:
 #     json.dump(output_data, outfile, indent=2)
--- test1.py
+++ test1.py
@@ -1,74 +1,37 @@
+import re
 import json
-import pandas as pd
+
+# Đọc dữ liệu từ tệp JSON
+with open('output.json', 'r') as json_file:
+    data_list = json.load(json_file)
+
+# Tạo một dictionary để lưu trữ kết quả
 
 
-def html_table_to_json(html_filename, json_filename):
-    # Read HTML file into a list of DataFrames
-    tables = pd.read_html(html_filename)
+# Hàm để thêm một mục vào dictionary kết quả
+def add_to_result(data,result_dict):
+    # Lấy giá trị từ trường "Name"
+    name = data.get('Name', '')
 
-    if not tables:
-        print("No tables found in the HTML file.")
-        return
+    # Tìm tất cả các ký tự và số trước dấu ngoặc
+    match = re.match(r'([^()]+)', name)
 
-    # Assuming the first table is the target table
-    target_table = tables[0]
+    # Kiểm tra xem có kết quả từ việc tìm kiếm không
+    if match:
+        # Lấy phần match đầu tiên (đã loại bỏ dấu ngoặc)
+        key = match.group(1).strip()
 
-    # Save the table to a JSON file
-    target_table.to_json(json_filename, orient='records', lines=False,indent=2,index=True)
+        # Lấy giá trị từ trường "id"
+        value = data.get('id')
 
-    with open(json_filename, 'r') as json_file:
-        data = json.load(json_file)
-    for i, item in enumerate(data):
-        item['id'] = i + 1
-    with open(json_filename, 'w') as output_file:
-        json.dump(data, output_file, indent=2)
-        
-        
+        # Thêm mục vào dictionary kết quả
+        result_dict[key] = value
 
-html_table_to_json('test_fillet.html', 'test.json')
 
-# Dữ liệu ban đầu
-with open("test.json", "r") as infile:
-    data = json.load(infile)
-print(data)
-# Tìm các điểm có "Definition" là "fillet13(D, B, E)" hoặc "fillet13(V, W, Z)"
-arc_data = [item for item in data if "fillet" in item.get("Definition")]
-print("data:=================")
-print(arc_data)
-# Tạo định dạng mới
-features = []
-for arc in arc_data:
-    if "Arc" in arc.get("Name"):
-      print(arc.get("id"))
-      start_point = next(item for item in arc_data if item.get("id") == arc.get("id") + 1)
-      end_point = next(item for item in arc_data if item.get("id") == arc.get("id") + 2)
-      origin_point = next(item for item in arc_data if item.get("id") == arc.get("id") + 3)
+result_dict = {}
+# Duyệt qua danh sách dữ liệu và thêm từng mục vào dictionary kết quả
+for data in data_list:
+    add_to_result(data,result_dict)
 
-      feature = {
-          "type": "Feature",
-          "geometry": {
-              "type": "Arc"
-          },
-          "properties": {
-              "id": arc["id"],
-              "startid": start_point["id"],
-              "endid": end_point["id"],
-              "originid": origin_point["id"],
-              "frame": "map"
-          }
-    }
-
-    features.append(feature)
-
-# Xuất ra file JSON mới
-output_data = {
-    "type": "FeatureCollection",
-    "features": features
-}
-
-print(features)
-
-# with open("output.json", "w") as outfile:
-#     json.dump(output_data, outfile, indent=2)
-
-print("Chuyển đổi hoàn tất. Dữ liệu được xuất ra file 'output.json'")
+# In ra kết quả
+print(result_dict)
